a39be934312f4f09978e18cd29a66ab90d01f29d
Ilham Ifham <ilhaamifham@gmail.com>
2024-08-31 18:38:33 +0530

    Updated mobile nav component

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -5,13 +5,25 @@ import { FaX } from "react-icons/fa6";
 const Aside = (props) => {
   return (
     <aside className={`bg-transparent-black text-white fixed h-dvh w-full top-0 full-hide opacity-0 duration-500 ${props.asideClassName}`} onClick={props.handleToggle}>
-      <nav className={`bg-black flex flex-col w-80 h-full p-4 -translate-x-full full-hide duration-500 ${props.navClassName}`} onClick={props.handleToggleStop}>
+      <nav className={`bg-black flex flex-col max-w-80 h-full p-4 -translate-x-full full-hide duration-500 ${props.navClassName}`} onClick={props.handleToggleStop}>
         <ul className="order-2 flex flex-col gap-6 mt-8 font-medium items-start overflow-y-auto">
-          <li className="self-stretch">
-            <Link to="/">Home</Link>
+          <li className="flex flex-row items-center justify-between self-stretch sm:hidden">
+            <Link to="/membership" className="sm:hidden bg-red-600 font-medium text-xs py-1 px-2 duration-500 hover:bg-white hover:text-black text-center uppercase">
+              Subscribe : 49.99/Year
+            </Link>
+            <Link to="/login" onClick={props.handleToggle}>
+              Log In
+            </Link>
           </li>
           <li className="self-stretch">
-            <Link to="/news">News</Link>
+            <Link to="/" onClick={props.handleToggle}>
+              Home
+            </Link>
+          </li>
+          <li className="self-stretch">
+            <Link to="/news" onClick={props.handleToggle}>
+              News
+            </Link>
           </li>
           <li className="self-stretch">
             <Link to="/business">Business</Link>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,57 +8,59 @@ const Header = (props) => {
   return (
     <header className="bg-black text-white sticky top-0">
       <div className="p-4 flex items-center max-w-7xl mx-auto">
-        <Link to="/" className="block min-w-32 max-w-48 order-2 px-4 mx-auto sm:order-3" >
+        <Link to="/" className="block min-w-32 max-w-48 order-2 px-4 mx-auto sm:order-3">
           <img src={Logo} alt="jadetimes logo" />
         </Link>
-        <button className="block order-1" onClick={props.handleToggle} aria-label="Menu" >
+        <button className="block order-1" onClick={props.handleToggle} aria-label="Menu">
           <FaAlignLeft size={24} />
         </button>
         <Link to="/search" className="block order-5" aria-label="search">
           <FaMagnifyingGlass size={24} />
         </Link>
-        <Link to="/membership" className=" hidden sm:block bg-red-600 font-medium text-xs py-1 px-2 order-2 ml-6 duration-500 hover:bg-white hover:text-black w-40 text-center uppercase" >Subscribe : 49.99/Year</Link>
-        <div className="w-40 hidden sm:block order-4 mr-6 fonmt-medium text-right">
+        <Link to="/membership" className=" hidden sm:block bg-red-600 font-medium text-xs py-1 px-2 order-2 ml-6 duration-500 hover:bg-white hover:text-black w-40 text-center uppercase">
+          Subscribe : 49.99/Year
+        </Link>
+        <div className="w-40 hidden sm:block order-4 mr-6 font-medium text-right">
           <Link to="/login">Log In</Link>
         </div>
       </div>
       <nav className="hidden bg-neutral-100 text-black sm:block">
-      <ul className="flex flex-row flex-wrap gap-6 p-2 justify-center text-sm font-medium">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/news">News</Link>
-        </li>
-        <li>
-          <Link to="/business">Business</Link>
-        </li>
-        <li>
-          <Link to="/sports">Sports</Link>
-        </li>
-        <li>
-          <Link to="/travel">Travel</Link>
-        </li>
-        <li>
-          <Link to="/culture">Culture</Link>
-        </li>
-        <li>
-          <Link to="/entertainment">Entertainment</Link>
-        </li>
-        <li>
-          <Link to="/innovation">Innovation</Link>
-        </li>
-        <li>
-          <Link to="/political">Political</Link>
-        </li>
-        <li>
-          <Link to="/universe">Universe</Link>
-        </li>
-        <li>
-          <Link to="/fashion">Fashion</Link>
-        </li>
-      </ul>
-    </nav>
+        <ul className="flex flex-row flex-wrap gap-6 p-2 justify-center text-sm font-medium">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/news">News</Link>
+          </li>
+          <li>
+            <Link to="/business">Business</Link>
+          </li>
+          <li>
+            <Link to="/sports">Sports</Link>
+          </li>
+          <li>
+            <Link to="/travel">Travel</Link>
+          </li>
+          <li>
+            <Link to="/culture">Culture</Link>
+          </li>
+          <li>
+            <Link to="/entertainment">Entertainment</Link>
+          </li>
+          <li>
+            <Link to="/innovation">Innovation</Link>
+          </li>
+          <li>
+            <Link to="/political">Political</Link>
+          </li>
+          <li>
+            <Link to="/universe">Universe</Link>
+          </li>
+          <li>
+            <Link to="/fashion">Fashion</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
