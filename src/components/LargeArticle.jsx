a5d550ef20ed6eef2eaece3bf015352e32fb52a9
Ilham Ifham <ilhaamifham@gmail.com>
2024-09-16 21:59:31 +0530

    Updated responsive design of home component

--- src/components/LargeArticle.jsx
+++ src/components/LargeArticle.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 const LargeArticle = () => {
     return (
-        <Link className="block border-b border-b-neutral-300 sm:border sm:border-neutral-300 sm:col-span-2 lg:col-span-4 xl:col-start-1 xl:col-end-9 xl:row-start-2 xl:row-end-5">
-            <article className="sm:grid sm:grid-cols-2 xl:h-full">
+        <Link className="block border-b border-b-neutral-300 sm:border sm:border-neutral-300 sm:col-span-2 lg:col-span-5 lg:row-start-1 lg:row-end-4 xl:col-start-1 xl:col-end-9 xl:row-start-2 xl:row-end-5">
+            <article className="sm:grid sm:grid-cols-2 lg:h-full">
                 <picture>
                     <img src="https://placehold.co/486x372" alt="" className="h-full" />
                 </picture>
                 <p className="text-white uppercase text-sm font-medium text-center absolute left-0 right-0 -translate-y-4 sm:hidden" aria-hidden="true"><span className="bg-neutral-950 inline-block px-2 py-1 border border-neutral-300">Latest Updates</span></p>
-                <div className="py-6 px-4 flex flex-col justify-between gap-2 lg:p-6 lg:justify-center">
+                <div className="py-6 px-4 flex flex-col justify-between gap-2 lg:p-6 sm:justify-center">
                     <h3 className="text-2xl">What is the outcome of U.S. National Security Advisor Jacob Sullivan's visit to China?</h3>
                     <p className="uppercase text-xs text-neutral-600">16 hours ago</p>
                 </div>
