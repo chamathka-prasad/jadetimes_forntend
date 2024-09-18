e3995add5897bc1f472c60ad9c83f145a7db8cd1
k.d. chamathka prasad kumara <chamathkaprasad123@gmail.com>
2024-09-18 23:14:25 +0530

    deploy_updates

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+  plugins: [react(),ghPages()],
   base:"/jadetimes_forntend/",
-  plugins: [react()],
 })
