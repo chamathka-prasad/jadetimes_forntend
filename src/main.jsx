b9e791923c03cdb6187f4b17d16a07efc3112ae6
Ilham Ifham <ilhaamifham@gmail.com>
2024-09-19 11:44:13 +0530

    Updated browser router

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/jadetimes_forntend">
       <App />
     </BrowserRouter>
   </StrictMode>
